968317a273ae71b193f447cb5dc451325b4d52f0
areif-dev <aj@ajreifsnyder.com>
2026-02-08 01:05:05 -0500

    custom builders for LineItem and Invoice

--- src/invoice.rs
+++ src/invoice.rs
@@ -20,14 +20,16 @@ where
 }
 
 #[derive(Debug, Builder, Serialize)]
-#[builder(setter(strip_option, into), pattern = "owned")]
+#[builder(setter(strip_option, into), build_fn(skip), pattern = "owned")]
 pub struct LineItem {
     sku: String,
     title: String,
     quantity: i32,
-
     #[serde(serialize_with = "serialize_bigdecimal")]
     price: BigDecimal,
+    #[builder(setter(skip))]
+    #[serde(serialize_with = "serialize_bigdecimal")]
+    total: BigDecimal,
 }
 
 #[derive(Debug, Builder, Serialize)]
@@ -53,23 +55,52 @@ pub struct Address {
 }
 
 #[derive(Debug, Builder, Serialize)]
-#[builder(setter(strip_option, into), pattern = "owned")]
+#[builder(setter(strip_option, into), pattern = "owned", build_fn(skip))]
 pub struct Invoice {
     id: String,
     #[serde(serialize_with = "serialize_datetime")]
-    #[builder(default = Local::now().into())]
     created_datetime: DateTime<FixedOffset>,
     #[serde(serialize_with = "serialize_datetime")]
-    #[builder(default = (Local::now() + Duration::days(30)).into())]
     net_due_datetime: DateTime<FixedOffset>,
     receiver: Party,
     sender: Party,
     logo: Option<PathBuf>,
-    #[builder(default = Vec::new())]
     line_items: Vec<LineItem>,
     #[serde(serialize_with = "serialize_bigdecimal")]
-    #[builder(default = BigDecimal::from(0))]
     paid: BigDecimal,
+    #[serde(serialize_with = "serialize_bigdecimal")]
+    #[builder(setter(skip))]
+    total: BigDecimal,
+    #[serde(serialize_with = "serialize_bigdecimal")]
+    #[builder(setter(skip))]
+    due: BigDecimal,
+    acct_id: Option<String>,
+    purchase_order: Option<String>,
+}
+
+impl LineItemBuilder {
+    pub fn build(self) -> Result<LineItem, LineItemBuilderError> {
+        let quantity = self
+            .quantity
+            .ok_or(LineItemBuilderError::UninitializedField("quantity"))?;
+        let price = self
+            .price
+            .ok_or(LineItemBuilderError::UninitializedField("price"))?;
+        let sku = self
+            .sku
+            .ok_or(LineItemBuilderError::UninitializedField("sku"))?;
+        let title = self
+            .title
+            .ok_or(LineItemBuilderError::UninitializedField("title"))?;
+
+        Ok(LineItem {
+            total: quantity * &price,
+            sku,
+            title,
+            quantity,
+            price,
+        })
+    }
 }
 
 impl LineItem {
@@ -89,8 +120,8 @@ impl LineItem {
         self.sku.to_string()
     }
 
-    pub fn total(&self) -> BigDecimal {
-        &self.price * self.quantity
+    pub fn total(&self) -> &BigDecimal {
+        &self.total
     }
 }
 
@@ -121,5 +152,43 @@ impl InvoiceBuilder {
                 ..self
             },
         }
+    }
+
+    pub fn build(self) -> Result<Invoice, InvoiceBuilderError> {
+        let id = self
+            .id
+            .ok_or(InvoiceBuilderError::UninitializedField("id"))?;
+        let created_datetime = self.created_datetime.unwrap_or(Local::now().into());
+        let net_due_datetime = self.net_due_datetime.unwrap_or(Local::now().into());
+        let receiver = self
+            .receiver
+            .ok_or(InvoiceBuilderError::UninitializedField("receiver"))?;
+        let sender = self
+            .sender
+            .ok_or(InvoiceBuilderError::UninitializedField("sender"))?;
+        let logo = self
+            .logo
+            .ok_or(InvoiceBuilderError::UninitializedField("logo"))?;
+        let line_items = self.line_items.unwrap_or(Vec::new());
+        let paid = self.paid.unwrap_or(BigDecimal::from(0));
+        let acct_id = self.acct_id.unwrap_or(None);
+        let purchase_order = self.purchase_order.unwrap_or(None);
+
+        let total: BigDecimal = line_items.iter().map(LineItem::total).sum();
+        let due = &total - &paid;
+        Ok(Invoice {
+            id,
+            created_datetime,
+            net_due_datetime,
+            receiver,
+            sender,
+            logo,
+            line_items,
+            paid,
+            due,
+            total,
+            acct_id,
+            purchase_order,
+        })
     }
 }
