c5b4241cffc2748f35e561994e30f6683d7f04b0
areif-dev <aj@ajreifsnyder.com>
2026-02-10 02:08:35 -0500

    add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,45 @@
+//! Utilities for generating professional invoices as PDFs
+//!
+//! This crate provides functions to render invoice templates and produce PDF files
+//! using a connection to headless chrome(ium). It exposes the invoice model types and a high-level
+//! function to generate a PDF from an [`Invoice`].
+//!
+//! # Example
+//!
+//! ```rust
+//! use invoice_pdf::{Invoice, InvoiceBuilder, generate_pdf, PartyBuilder, AddressBuilder};
+//!
+//! let inv = InvoiceBuilder::default()
+//!     .id("1")
+//!     .logo("./logo.png")
+//!     .receiver(
+//!         PartyBuilder::default()
+//!             .name("A")
+//!             .address(
+//!                 AddressBuilder::default()
+//!                 .line1("1 street st")
+//!                 .city("city")
+//!                 .province_code("PR")
+//!                 .postal_code("Post")
+//!                 .build().unwrap()
+//!             )
+//!             .build().unwrap())
+//!     .sender(
+//!         PartyBuilder::default()
+//!             .name("B")
+//!             .address(
+//!                 AddressBuilder::default()
+//!                 .line1("1 street st")
+//!                 .city("city")
+//!                 .province_code("PR")
+//!                 .postal_code("Post")
+//!                 .build().unwrap()
+//!             )
+//!         .build().unwrap())
+//!     .build().unwrap();
+//! generate_pdf(&inv);
+//! ```
+
 pub mod error;
 pub mod invoice;
 pub mod template_env;
@@ -27,6 +69,69 @@ pub async fn print() -> Result<(), crate::Error> {
     let client = ClientBuilder::native()
         .capabilities(caps)
         .connect("http://localhost:4444")
+        .await
+}
+
+/// Generate a PDF byte array from [`Invoice`]
+///
+/// This function renders an HTML template from the provided [`Invoice`],
+/// navigates a headless browser to the rendered HTML, prints the page as a PDF, and returns the
+/// resulting byte array
+///
+/// # Arguments
+///
+/// - `invoice`: Reference to the [`Invoice`] to render and print.
+///
+/// # Returns
+///
+/// - The byte array representing the PDF if successful
+///
+/// # Errors
+///
+/// Returns `Err(crate::Error)` if any step fails:
+/// - connecting to the headless browser [`Client`]
+/// - setting up the templating environment
+/// - rendering the HTML template
+/// - navigating the browser to the generated data URL
+/// - configuring the print job or printing to PDF
+///
+/// # Example
+///
+/// ```rust
+/// use invoice_pdf::{Invoice, InvoiceBuilder, PartyBuilder, AddressBuilder, generate_pdf};
+///
+/// let inv = InvoiceBuilder::default()
+///     .id("1")
+///     .logo("./logo.png")
+///     .receiver(
+///         PartyBuilder::default()
+///             .name("A")
+///             .address(
+///                 AddressBuilder::default()
+///                 .line1("1 street st")
+///                 .city("city")
+///                 .province_code("PR")
+///                 .postal_code("Post")
+///                 .build().unwrap()
+///             )
+///             .build().unwrap())
+///     .sender(
+///         PartyBuilder::default()
+///             .name("B")
+///             .address(
+///                 AddressBuilder::default()
+///                 .line1("1 street st")
+///                 .city("city")
+///                 .province_code("PR")
+///                 .postal_code("Post")
+///                 .build().unwrap()
+///             )
+///         .build().unwrap())
+///     .build().unwrap();
+/// generate_pdf(&inv);
+/// ```
+pub async fn generate_pdf(invoice: &Invoice) -> Result<Vec<u8>, crate::Error> {
+    let client = connect_to_client()
         .await
         .map_err(crate::Error::from)
         .add_context("connecting to client")
