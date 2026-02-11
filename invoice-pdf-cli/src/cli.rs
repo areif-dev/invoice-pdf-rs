5c0fa26265acd70d786b72cdec66617f73f693a3
areif-dev <aj@ajreifsnyder.com>
2026-02-11 16:40:24 -0500

    cli: finish initial implementation

--- invoice-pdf-cli/src/cli.rs
+++ invoice-pdf-cli/src/cli.rs
@@ -1,21 +1,45 @@
-use std::path::PathBuf;
+use std::{
+    fs,
+    io::{self, Read},
+    path::PathBuf,
+};
 
 use clap::Parser;
-use invoice_pdf::Invoice;
+use invoice_pdf::{Invoice, error::AddContext};
+
+fn read_until_eof() -> io::Result<String> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    Ok(input)
+}
 
 #[derive(Debug, Parser)]
 pub struct Cli {
     /// Path to the JSON file with invoice data to print
     #[arg(short, long)]
-    data: Option<PathBuf>,
+    pub data: Option<PathBuf>,
 
     /// Path to the directory where PDF outputs should be saved
     #[arg(short, long)]
-    out: Option<PathBuf>,
+    pub out: Option<PathBuf>,
 }
 
 impl Cli {
-    pub fn get_data(&self) -> Result<Vec<Invoice>, invoice_pdf::Error> {
-        Ok(Vec::new())
+    pub fn get_invoices(&self) -> Result<Vec<Invoice>, invoice_pdf::Error> {
+        let raw = match &self.data {
+            Some(path) => fs::read_to_string(path)
+                .map_err(invoice_pdf::Error::from)
+                .add_context(&format!(
+                    "reading invoice data from file '{}'",
+                    path.to_str().unwrap_or("UNKNOWN")
+                ))?,
+            None => read_until_eof()
+                .map_err(invoice_pdf::Error::from)
+                .add_context("reading invoice data from stdin")?,
+        };
+
+        Ok(serde_json::from_str(&raw)
+            .map_err(|e| invoice_pdf::Error::from(format!("{:?}", e)))
+            .add_context("parsing invoice JSON")?)
     }
 }
--- invoice-pdf-cli/src/main.rs
+++ invoice-pdf-cli/src/main.rs
@@ -1,9 +1,74 @@
+use std::{io::Write, process::Child};
+
 use clap::Parser;
+use invoice_pdf::{Invoice, error::AddContext, generate_pdf, start_chromedriver};
 
 use crate::cli::Cli;
 
 mod cli;
 
-fn main() {
+fn kill_chrome(chrome_process: &mut Child) -> Result<(), invoice_pdf::Error> {
+    chrome_process
+        .kill()
+        .map_err(invoice_pdf::Error::from)
+        .add_context("killing chromedriver process from cli")?;
+    Ok(())
+}
+
+async fn write_invoice_pdf(invoice: &Invoice, cli: &Cli) -> Result<(), invoice_pdf::Error> {
+    let data = generate_pdf(invoice)
+        .await
+        .add_context("generating pdf data from invoice")
+        .add_context(&format!("invoice id: {}", invoice.id()))?;
+    match &cli.out {
+        Some(out) => {
+            let path = out.join(format!("{}.pdf", invoice.id()));
+            if std::fs::write(&path, data).is_err() {
+                eprintln!(
+                    "Failed to write invoice '{}' to '{}'",
+                    invoice.id(),
+                    &path.to_string_lossy()
+                );
+                write_invoice_pdf_to_stdout(invoice).await
+            } else {
+                Ok(())
+            }
+        }
+        None => write_invoice_pdf_to_stdout(invoice).await,
+    }
+}
+
+async fn write_invoice_pdf_to_stdout(invoice: &Invoice) -> Result<(), invoice_pdf::Error> {
+    let mut buf = generate_pdf(invoice)
+        .await
+        .add_context("generating invoice pdf")
+        .add_context("printing to stdout")?;
+    std::io::stdout()
+        .write_all(&mut buf)
+        .map_err(invoice_pdf::Error::from)
+        .add_context("writing invoice pdf to stdout")?;
+    std::io::stdout()
+        .flush()
+        .map_err(invoice_pdf::Error::from)
+        .add_context("flushing stdout")
+        .add_context("printing to stdout")?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), invoice_pdf::Error> {
+    let mut chrome_process = start_chromedriver().add_context("starting chromedriver in cli")?;
     let cli = Cli::parse();
+    let invoices = cli
+        .get_invoices()
+        .or_else(|e| {
+            kill_chrome(&mut chrome_process)?;
+            Err(e)
+        })
+        .add_context("deserializing invoices from cli")?;
+    for invoice in invoices {
+        write_invoice_pdf(&invoice, &cli).await?;
+    }
+    kill_chrome(&mut chrome_process)?;
+    Ok(())
 }
